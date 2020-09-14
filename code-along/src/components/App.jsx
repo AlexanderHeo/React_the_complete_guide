440911a6bc91261024078f8f533f6a9112241ea8
AlexanderHeo <jikster712@gmail.com>
2020-09-14 16:28:48 -0700

    styling components

--- code-along/src/components/App.jsx
+++ code-along/src/components/App.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
+
+
+const StyledButton = styled.button`
+  background-color: ${props => props.clickedStyle ? 'red' : 'green'};
+  color: #eee;
+  font: inherit;
+  border: 3px solid #00ffff;
+  padding: 8px;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.clickedStyle ? 'pink' : 'lightgreen'};
+    color: #222;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -56,14 +73,19 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '3px solid blue',
-      borderRadius: '5px',
-      cursor: 'pointer',
-      padding: '8px'
-    };
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: '#eee',
+    //   font: 'inherit',
+    //   border: '3px solid #00ffff',
+    //   borderRadius: '5px',
+    //   cursor: 'pointer',
+    //   padding: '8px',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: '#222',
+    //   }
+    // };
 
     let personsList = (<p>Click the button</p>);
     if (this.state.isPersonDisplayed) {
@@ -79,15 +101,31 @@ class App extends Component {
           })}
         </div>
       );
+      // style.backgroundColor = "#ff0000";
+      // style.border = "3px solid #ff00ff";
+      // style[':hover'] = {
+      //   backgroundColor: '#ff00ff',
+      //   color: '#0022ff'
+      // }
+    }
+
+    const colorClass = [];
+    if (this.state.persons.length <= 2) {
+      colorClass.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      colorClass.push('bold');
     }
 
     return (
       <div className="App">
         <h1>React - The Complete Guide: code-along</h1>
-        <button
-          style={style}
+        <p className={colorClass.join(' ')}>by Max, on Udemy</p>
+        <StyledButton
+          // style={style}
+          clickedStyle={this.state.isPersonDisplayed}
           onClick={this.togglePersons}>Show/Hide Persons
-        </button>
+        </StyledButton>
         {personsList}
       </div>
     )
