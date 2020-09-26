a78bcf2634193335a4a884a65beafaf327b7eaaf
AlexanderHeo <jikster712@gmail.com>
2020-09-26 16:57:00 -0700

    dynamically calculate ingredient sum

--- take-two/src/components/Burger/Burger.js
+++ take-two/src/components/Burger/Burger.js
@@ -3,7 +3,7 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])]
         .map((_, i) => {
@@ -12,21 +12,37 @@ const burger = (props) => {
             type={ingredientKey}
           />
         })
-    });
-
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
     /*
     ingredients: {
       salad: 1, bacon:1, cheese: 2, meat: 3
     }
-    console.log(transformedIngredients);
+    console.log(transformedIngredients); // before reduce
     [
-      { key: salad0 },
-      { key: bacon0 },
-      { key: cheese0 }, { key: cheese1 },
-      { key: meat0 }, { key: meat1 }, { key: meat2 },
+      [{ key: salad0 }],
+      [{ key: bacon0 }],
+      [{ key: cheese0 }, { key: cheese1 }],
+      [{ key: meat0 }, { key: meat1 }, { key: meat2 }]
     ];
     */
-
+   /*
+   console.log(transformedIngredients); // after reduce
+   [
+     {key: salad0},
+     {key: bacon0},
+     {key: cheese0},
+     {key: cheese1},
+     {key: meat0},
+     {key: meat1},
+     {key: meat2},
+   ]
+    */
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
