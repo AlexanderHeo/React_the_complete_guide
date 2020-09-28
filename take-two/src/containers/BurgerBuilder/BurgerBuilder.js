6380b3b3717b0b02b0960897a1962028205902ed
AlexanderHeo <jikster712@gmail.com>
2020-09-28 12:55:05 -0700

    build controls created, add button created

--- take-two/src/containers/BurgerBuilder/BurgerBuilder.js
+++ take-two/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Burger from '../../components/Burger/Burger';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
   state={
@@ -8,14 +16,37 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
+  }
+
+  handleAddIngredients = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {...this.state.ingredients};
+    updatedIngredients[type] = updatedCount;
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients
+    })
+  }
+
+  handleRemoveIngredients = (type) => {
+
   }
 
   render() {
     return (
       <>
         <Burger ingredients={this.state.ingredients}/>
-        <div>Build Controls</div>
+        <BuildControls
+          ingredientAdded={this.handleAddIngredients}
+        />
       </>
     )
   }
