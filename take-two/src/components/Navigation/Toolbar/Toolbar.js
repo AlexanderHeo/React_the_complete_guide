9602270e250396e8e7495b8f0345fccf81c86849
AlexanderHeo <jikster712@gmail.com>
2020-09-28 17:05:52 -0700

    navi component for desktop created

--- take-two/src/components/Navigation/Toolbar/Toolbar.js
+++ take-two/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Toolbar.css';
 
 const toolbar = (props) => (
@@ -7,9 +8,7 @@ const toolbar = (props) => (
     <div>Hamburger Menu</div>
     <Logo />
     <nav>
-      <ul>
-        links
-      </ul>
+      <NavigationItems />
     </nav>
   </header>
 );
