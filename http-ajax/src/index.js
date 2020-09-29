222016d15fea956e39b5caabc58f034594c10658
AlexanderHeo <jikster712@gmail.com>
2020-09-29 14:53:15 -0700

    setup axios interceptors for request and response

--- http-ajax/src/index.js
+++ http-ajax/src/index.js
@@ -1,8 +1,27 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+axios.interceptors.request.use(requestConfig => {
+  console.log(requestConfig);
+  // modify requestConfig, such as adding a header here
+  return requestConfig;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(responseConfig => {
+  console.log(responseConfig);
+  //modify responseConfig here
+  return responseConfig;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
