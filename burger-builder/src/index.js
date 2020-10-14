2cb1b04ec4db1635edcc212e44d5f70758981198
AlexanderHeo <jikster712@gmail.com>
2020-10-14 11:14:16 -0700

    basic redux setup - store setup, Provider wraps App, reducer and actions files ready

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore } from 'redux';
 import App from './App';
 import './index.css';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 class ReactApp extends React.Component {
   render() {
     return (
-      <Router>
-        <App />
-      </Router>
+			<Provider store={store}>
+				<Router>
+					<App />
+				</Router>
+			</Provider>
     );
   }
 }
